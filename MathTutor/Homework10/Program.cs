d6a412e28a5728160632cbc7dbc54ae3fa35169c
Ростислав Шведов <rustysquezzy@outlook.com>
2024-05-03 09:43:29 +0300

    Initial commit.

---
+++ MathTutor/Homework10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
